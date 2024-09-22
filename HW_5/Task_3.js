86297259e2dc48b717bd82f49bcc39732cfa8cc3
Alexey_Bodyagin <azartnyi7@gmail.com>
2024-09-22 14:04:54 +0300

    added HW5 with Tasks 1,2,3

---
+++ HW_5/Task_3.js
@@ -0,0 +1,31 @@
+// Создать программу, которая будет принимать на вход СЛОВО (создать переменную со словом), 
+//   и выводить в консоль количество гласных и согласных букв в этом слове. 
+//   Ответ должен выводиться шаблонным литералом вида word contains x vowels and y consonants
+
+const vowels = 'aeiou'
+const consonants = 'bcdfghjklmnpqrstvwxyz'
+let vowelsCount = 0;
+let consonantsCount = 0;
+let word = 'testword';
+
+
+for (let i = 0; i < word.length; i++) {
+    for (let j = 0 ; j < vowels.length; j++) {
+        if (word[i] === vowels[j] ) {
+            vowelsCount++;
+           // console.log(`Буква ${word[i]} гласная ${vowels[j]}`);;
+        } 
+    }
+  }
+
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0 ; j < consonants.length; j++) {
+        if (word[i] === consonants[j] ) {
+            consonantsCount++;
+           // console.log(`Буква ${word[i]} согласная ${consonants[j]}`);; 
+        } 
+    }
+  }
+
+  console.log(`Word "${word}" contains ${vowelsCount} vowels and ${consonantsCount} consonants`);
