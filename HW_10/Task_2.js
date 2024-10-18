e0eda79724982bf0155a2de1e031ee1ef212b68d
Alexey_Bodyagin <azartnyi7@gmail.com>
2024-10-19 00:28:47 +0300

    refactored HW_10 with Tasks 2 (added reduce)

--- HW_10/Task_2.js
+++ HW_10/Task_2.js
@@ -6,17 +6,20 @@ const numbers = [1, 2, 2, 3, 4, 4, 4, 5];
 // Ожидается: { 1: 1, 2: 2, 3: 1, 4: 3, 5: 1 }
 
 function countOccurrences(arr) {
-let obj = {};
-
-arr.forEach((element) => { 
-if (obj[element]) {
-    obj[element]++;
-} 
-else {
-obj[element] = 1;
+return arr.reduce((acc, element) => {
+    acc[element] = acc[element] ? acc[element] + 1 : 1;
+    return acc;
+  }, {});
 }
-});
-return obj;
-}
-
 console.log(countOccurrences(numbers));
+
+// let obj = {};
+// arr.forEach((element) => { 
+// if (obj[element]) {
+//     obj[element]++;
+// } 
+// else {
+// obj[element] = 1;
+// }
+// });
+// return obj;
